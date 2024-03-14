0ffbeee2743eb24dc76fcb323eb103cd8690bb34
VitorHmaia <vitorhmaia@outlook.com>
2024-03-14 20:37:47 -0300

    chore: define base dir to templates and weather entity
    entity without django orm

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# weather/models.py
+class WeatherEntity:
+
+    def __init__(self, temperature, date,
+                 city='', atmosphericPressure='',
+                 humidity='', weather='') -> None:
+        self.temperature = temperature
+        self.city = city
+        self.atmosphericPressure = atmosphericPressure
+        self.humidity = humidity
+        self.weather = weather
+        self.date = date
+
+    def __str__(self) -> str:
+        return (f"Weather <{self.temperature}>")
