d96bd29715a22cfec7bcb08fbd73dbc4deb69a55
VitorHmaia <vitorhmaia@outlook.com>
2024-03-19 13:24:21 -0300

    chore: create class WeatherView to call repository funcion and render home.html
    WeatherGenerate gets repository in django by collection name

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from datetime import datetime
+from random import randrange
+from django.views import View
+from django.shortcuts import render, redirect
+from .models import WeatherEntity
+from .repositories import WeatherRepository
+
+class WeatherView(View):
+    def get(self, request):
+        repository = WeatherRepository(collectionName='weathers')
+        weathers = repository.getAll()
+        return render(request, "home.html", {"weathers":weathers})
+    
+
+class WeatherGenerate(View):
+    def get(self, request):
+        repository = WeatherRepository(collectionName='weathers')
+        wheater = {
+            "temperature" : 28,
+            "date": "hoje"
+            }
+        repository.insert(wheater)
+
+        return redirect('Weather View')
