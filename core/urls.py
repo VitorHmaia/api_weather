0d4e137f1f126814e1f042023672044394643435
VitorHmaia <vitorhmaia@outlook.com>
2024-04-16 20:26:52 -0300

    chore(urls): create routes to call weather classes

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,14 @@
 #urls.py
 from django.urls import path
-from weather.views import WeatherView, WeatherGenerate, WeatherReset
+from weather.views import WeatherView, WeatherGenerate, WeatherClear, WeatherInsert, WeatherEdit, WeatherRemove
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', WeatherView.as_view(), name='Weather View'),
-    path('generate', WeatherGenerate.as_view(), name='Weather Generate'),
-    path('reset', WeatherReset.as_view(), name='Weather Reset'),
+    path('generate', WeatherGenerate.as_view(), name='Generate Weather'),
+    path('insert', WeatherInsert.as_view(), name='Insert Weather'),
+    path('clear', WeatherClear.as_view(), name='Clear Database'),
+    path('edit/<str:id>', WeatherEdit.as_view(), name='Weather Edit'),
+    path('remove/<str:id>', WeatherRemove.as_view(), name='Weather Remove')
 ]
