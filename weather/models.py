7a8817011794448ee0bb53368635e08f72844d6c
VitorHmaia <vitorhmaia@outlook.com>
2024-04-15 22:47:11 -0300

    chore: create get attr function to return entity object

--- weather/models.py
+++ weather/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # weather/models.py
 class WeatherEntity:
 
-    def __init__(self, temperature, date,
-                 city='', atmospheric_pressure='',
-                 humidity='', weather='') -> None:
+    def __init__(self, temperature, city, atmospheric_pressure, humidity, weather, date, id=None) -> None:
+        self.id = id
         self.temperature = temperature
         self.city = city
         self.atmospheric_pressure = atmospheric_pressure
@@ -13,5 +12,12 @@ class WeatherEntity:
         self.weather = weather
         self.date = date
 
+        
     def __str__(self) -> str:
         return (f"Weather <{self.temperature}>")
+    
+    def __getattribute__(self, __name: str) -> Any:
+        if (__name=='date'):
+            return object.__getattribute__(self, __name).strftime("%d/%m/%Y %H:%M:%S")
+        else:
+            return object.__getattribute__(self, __name)
